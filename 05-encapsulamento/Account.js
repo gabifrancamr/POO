87ffcb67400894add7546e50a4252a42b6569454
Gabriela França <gabrielafranca2008@hotmail.com>
2022-11-07 08:28:16 -0300

    encapsulamento

--- 05-encapsulamento/Account.js
+++ 05-encapsulamento/Account.js
@@ -1,31 +1,31 @@
 class Account {
-    #password
-    #balance = 0
-    constructor(user) {
-        this.email = user.email
-        this.#password = user.password
-        //this.#balance = 0
-    }
+  #password;
+  #balance = 0;
+  constructor(user) {
+    this.email = user.email;
+    this.#password = user.password;
+    //this.#balance = 0
+  }
 
-    getBalance(email, password){
-        if(this.#authenticate(email, password)) {
-            return this.#balance
-        } else {
-            return null
-        }
+  getBalance(email, password) {
+    if (this.#authenticate(email, password)) {
+      return this.#balance;
+    } else {
+      return null;
     }
+  }
 
-    #authenticate(email, password){
-        return this.email === email && this.#password === password
-    }
+  #authenticate(email, password) {
+    return this.email === email && this.#password === password;
+  }
 }
 
 const user = {
-    email: 'isaac@gmail.com',
-    password: '1234'
-}
+  email: "isaac@gmail.com",
+  password: "1234",
+};
 
-const myAccount = new Account(user)
+const myAccount = new Account(user);
 
-console.log(myAccount)
-console.log(myAccount.getBalance('isaac@gmail.com', '1234'))
+console.log(myAccount);
+console.log(myAccount.getBalance("isaac@gmail.com", "1234"));
